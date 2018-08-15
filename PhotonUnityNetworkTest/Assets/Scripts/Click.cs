59c7834b1c41dddea3420a226a4c6fa40d68ddc2
bryan <910223092@qq.com>
2018-08-15 18:33:50 +0800

    add

---
+++ PhotonUnityNetworkTest/Assets/Scripts/Click.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using ExitGames.Client.Photon;
+using UnityEngine.UI;
+public class Click : MonoBehaviour
+{
+    public GameObject prefab;
+  //  public Button btn ;
+    public GameObject ga;
+    void Start()
+    {
+         Button btn = this.GetComponent<Button> ();
+        btn.onClick.AddListener (OnClick);
+    }
+
+    
+    void OnClick()
+    {
+        Debug.Log(InputToEvent.inputHitPos);
+        ga =  PhotonNetwork.Instantiate(prefab.name, InputToEvent.inputHitPos, Quaternion.identity, 0);
+
+    }
+
+
+    private void Update() {
+        if (Input.GetKey(KeyCode.DownArrow)){
+                ga.transform.Rotate(Vector3.up);
+        }
+    }
+
+/* 
+    void Start () {
+        Button btn = this.GetComponent<Button> ();
+        btn.onClick.AddListener (OnClick);
+    }
+
+    private void OnClick(){
+        Debug.Log ("Button Clicked. ClickHandler.");
+    }
+
+    */
+
+}
+
---
+++ PhotonUnityNetworkTest/Assets/Scripts/PUNConnect.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PUNConnect : MonoBehaviour {
+
+
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings("v1.0");
+    }
+    void OnGUI()
+    {
+        GUI.Label(new Rect(Screen.width / 2 - 75, 20, 150, 20), PhotonNetwork.connectionStateDetailed.ToString());
+        if (GUI.Button(new Rect(Screen.width/2-50,50,100,30),"加入游戏房间" ))
+        {
+            if (PhotonNetwork.connected)
+            {
+                PhotonNetwork.JoinOrCreateRoom("xiaogeformax", new RoomOptions {MaxPlayers = 16}, null);
+            }
+
+        }
+
+        if (GUI.Button(new Rect(Screen.width / 2 - 50, 90, 100, 30), "退出游戏房间"))
+        {
+            if (PhotonNetwork.connected)
+            {
+                PhotonNetwork.LeaveRoom();
+            }
+        }
+
+        if (GUI.Button(new Rect(Screen.width / 2 - 50, 130, 100, 30), "Connect"))
+        {
+            if (PhotonNetwork.connectionStateDetailed == ClientState.PeerCreated)
+            {
+                PhotonNetwork.ConnectToBestCloudServer("v1.0");
+            }
+        }
+    }
+}
